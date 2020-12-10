1fdf1d6ee6e5e7a2d349e786f9d52964a684f165
Felix Chin <felixmchin@gmail.com>
2020-12-09 23:05:06 -0800

    minor update to styling

--- src/components/show-list-item.jsx
+++ src/components/show-list-item.jsx
@@ -10,7 +10,7 @@ const ShowListItem = ({ id, title, desc, image, setView, getDetails }) => {
   return (
     <div className="d-flex flex-row bg-light-grey round-corners show-list-item">
       <img src={image} alt="show poster" className="round-corners poster ml-4 mr-5"/>
-      <div className="d-flex flex-column justify-content-center align-items-center">
+      <div className="d-flex flex-column justify-content-center">
         <h4 className="font-weight-bold">{title}</h4>
         <p className="summary-text text-muted">{desc}</p>
         <div>
