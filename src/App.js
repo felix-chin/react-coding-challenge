d6887088728ae873c3df1d7dda388c92281b8f58
Felix Chin <felixmchin@gmail.com>
2020-12-09 19:36:23 -0800

    added header and search bar components

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const App = (movie) => {
 
   return (
     <div>
+      <Header />
     </div>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,6 +3,13 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
+      <div className="navbar navbar-dark bg-dark-blue">
+        <div className="container">
+          <div className="navbar-brand">
+            <h5>Show Finder</h5>
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
